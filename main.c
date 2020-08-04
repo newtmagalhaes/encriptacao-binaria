80658f88a14cfb621f8210ff04a2db3acfea4126
Beloin Sena <beloin.rodrigues@gmail.com>
2020-08-04 18:01:24 -0300

    Separaçao do trabalho

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 int main()
 {
+    // CLI:
+    // @returns char[] Senha, char[] Chave
+
+    // Operação Bitwise:
+    // @param char[] Senha, char[] Chave
+    // @returns char[] bitwiseChar
+
+    // Arvore:
+    // @param char[] char
+    // @returns char[] encriptedChar
     return 0;
 }
