b2f91de9c8997a0cffd7743fc9410f686880be9b
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-08-13 13:59:45 -0300

    Adicionando funções freeTree, filterStr e comentários

--- headers/tree.h
+++ headers/tree.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define TREE_NULL_CHAR '#'
+#define _TREE_NULL_CHAR '#'
 
 /**
  *    Implementando estrutura de dados de Árvore
@@ -11,13 +11,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct node
+typedef struct _node
 {
-    struct node *leftNode, *rightNode;
+    struct _node *leftNode, *rightNode;
     char data;
 } Tree;
 
-Tree * createNode()
+Tree * _createNode()
 {
     Tree *newNode = NULL;
     do
@@ -32,6 +32,7 @@ Tree * createNode()
     return newNode;
 }
 
+// Dado um nodeArr retorna um ponteiro para um novo nodeArr com o dobro do tamanho da entrada
 Tree ** _getNextNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
 {
     Tree **nextNodeArr = (Tree **)realloc(NULL, 2 * nodeArrLength * sizeof(Tree));
@@ -51,6 +52,31 @@ Tree ** _getNextNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
     return nextNodeArr;
 }
 
+// retorna uma nova str sem o caractere desejado
+char *_filterStr(char str[], char reject)
+{
+    int j = 0;
+    char *newStr = NULL;
+
+    do
+    {
+        newStr = (char *)malloc(strlen(str));
+    } while (newStr == NULL);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != reject)
+        {
+            newStr[j] = str[i];
+            j++;
+        }
+    }
+    newStr[j] = '\0';
+
+    return (char *)realloc(newStr, strlen(newStr));
+}
+
+// Dado um nodeArr retorna uma string com os caracteres de cada nó
 char *_getCharArrFromNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
 {
     unsigned charArrLength = (nodeArrLength + 1) * sizeof(char);
@@ -63,15 +89,15 @@ char *_getCharArrFromNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
 
     for (int i = 0; i < nodeArrLength; i++)
     {
-        charArr[i] = nodeArr[i] != NULL? nodeArr[i]->data : TREE_NULL_CHAR;
+        charArr[i] = nodeArr[i] != NULL? nodeArr[i]->data : _TREE_NULL_CHAR;
     }
     charArr[nodeArrLength] = '\0';
 
     return charArr;
 }
 
-// Verifica se dado um array de (Tree *) há pelo menos 1 ponteiro diferente de NULL
-int _isCurrNodeArrNotNull(Tree *nodeArr[], unsigned nodeArrLength)
+// Dado um nodeArr verifica se há pelo menos 1 nó diferente de NULL
+int _isNodeArrNotNull(Tree *nodeArr[], unsigned nodeArrLength)
 {
     for (int i = 0; i < nodeArrLength; i++)
     {
@@ -83,6 +109,7 @@ int _isCurrNodeArrNotNull(Tree *nodeArr[], unsigned nodeArrLength)
     return 0;
 }
 
+// limpa as posições no nodeArr e desfaz a alocação
 void _freeNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
 {
     for (int i = 0; i < nodeArrLength; i++)
@@ -92,15 +119,42 @@ void _freeNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
     free(nodeArr);
 }
 
+// Dado um nodeArr exibe os caracteres de cada nó no console
 void _printNodeArr(Tree *nodeArr[], unsigned nodeArrLength)
 {
     for (int i = 0; i < nodeArrLength; i++)
     {
-        printf("\t%c", nodeArr[i] != NULL? nodeArr[i]->data : TREE_NULL_CHAR);
+        printf("\t%c", nodeArr[i] != NULL? nodeArr[i]->data : _TREE_NULL_CHAR);
     }
     printf("\n");
 }
 
+// Desfaz a alocação de um nó e de seus filhos
+void freeTree(Tree *node)
+{
+    if (node == NULL)
+    {
+        printf("no node to free!\n");
+    }
+    else
+    {
+        if (node->leftNode != NULL)
+        {
+            freeTree(node->leftNode);
+            node->leftNode = NULL;
+        }
+
+        if (node->rightNode != NULL)
+        {
+            freeTree(node->rightNode);
+            node->rightNode = NULL;
+        }
+
+        free(node);
+    }
+}
+
+// Exibe os caracteres da árvore no console
 void printTree(Tree *root)
 {
     if (root == NULL)
@@ -125,11 +179,12 @@ void printTree(Tree *root)
 
             currNodeArr = nextNodeArr;
             nextNodeArr = _getNextNodeArr(currNodeArr, currLength);
-        } while (_isCurrNodeArrNotNull(currNodeArr, currLength));
+        } while (_isNodeArrNotNull(currNodeArr, currLength));
         printf("Tree end!\n");
     }
 }
 
+// Faz a inserção binária na árvore
 void binaryInsert(Tree *root, char insertData)
 {
     if (root->data == '\0')
@@ -138,11 +193,11 @@ void binaryInsert(Tree *root, char insertData)
     }
     else
     {
-        if (insertData <= root->data)
+        if (insertData < root->data)
         {
             if (root->leftNode == NULL)
             {
-                root->leftNode = createNode();
+                root->leftNode = _createNode();
             }
             binaryInsert(root->leftNode, insertData);
         }
@@ -150,13 +205,14 @@ void binaryInsert(Tree *root, char insertData)
         {
             if (root->rightNode == NULL)
             {
-                root->rightNode = createNode();
+                root->rightNode = _createNode();
             }
             binaryInsert(root->rightNode, insertData);
         }
     }
 }
 
+// retorna uma string com os caracteres da árvore
 char *treeToStr(Tree *root)
 {
     if (root == NULL)
@@ -190,17 +246,16 @@ char *treeToStr(Tree *root)
             currLength *= 2;
             currNodeArr = nextNodeArr;
             nextNodeArr = _getNextNodeArr(currNodeArr, currLength);
-        } while (_isCurrNodeArrNotNull(currNodeArr, currLength));
+        } while (_isNodeArrNotNull(currNodeArr, currLength));
 
         return str;
     }
 }
 
+// retorna uma árvore com a string inserida de forma binária
 Tree *strToTree(char *str)
 {
-    Tree *root = createNode();
-
-    int i = 0;
+    Tree *root = _createNode();
 
     for (int i = 0; str[i] != '\0'; i++)
     {
